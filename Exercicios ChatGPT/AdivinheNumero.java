58dcafcaf471d81856a429ad1be7d112e85e0cf4
fmarques91 <francisco.marques@fatec.sp.gov.br>
2024-12-27 17:25:10 -0300

    Exercicios 6 e 7 finalizados

--- Exercicios ChatGPT/AdivinheNumero.java
+++ Exercicios ChatGPT/AdivinheNumero.java
@@ -1,4 +1,45 @@
 // 7. Jogo: Adivinhe o Número
-// 	• Descrição: Faça um programa que gera um número aleatório entre 1 e 100 e permite que o usuário tente adivinhar. Diga ao usuário se o palpite é muito alto ou baixo.
+// 	• Descrição: Faça um programa que gera um número aleatório entre 1 e 100 e permite que o usuário tente adivinhar. Diga ao usuário
+// se o palpite é muito alto ou baixo.
 // 	• Objetivo: Trabalhar com números aleatórios e controle de loops.
 // Desafio extra: Exiba o número de tentativas usadas e permita jogar novamente.
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class AdivinheNumero{
+    public static void main(String[] args) {
+        int num = 100;
+        int tentativas = 0;
+        Scanner scan = new Scanner(System.in);
+        Random random = new Random();
+        int numAleatorio = random.nextInt(100) + 1;
+
+        while(num > 0){
+            System.out.print("Digite um número inteiro entre 0 e 100 para continuar, ou um negativo para Sair: ");
+            num = scan.nextInt();
+            scan.nextLine();
+
+            if(num >= 0 && num <= 100){
+                tentativas++;
+                if(num < numAleatorio){
+                    System.out.println("O número: " + num + ", é menor que o número aleatório e você já fez " + tentativas + " tentativas!");
+                }
+                else if(num > numAleatorio){
+                    System.out.println("O número: " + num + ", é maior que o número aleatório e você já fez " + tentativas + " tentativas!");
+                }
+                else if(num == numAleatorio){
+                    System.out.println("Parabéns o número aleatório é " + numAleatorio + ", você acertou!");
+                    num = -1;
+                }
+            }
+            else if(num > 0){
+                System.out.println(num + " não é uma opção válida!");
+            }
+            else{
+                System.out.println("Fechando o programa!");
+            }
+        }
+        scan.close();
+    }
+}
--- Exercicios ChatGPT/OrdenacaoSimples.java
+++ Exercicios ChatGPT/OrdenacaoSimples.java
@@ -2,3 +2,44 @@
 // 	• Descrição: Solicite ao usuário três números e exiba-os em ordem crescente.
 // 	• Objetivo: Trabalhar com lógica de ordenação.
 // Desafio extra: Permita que o usuário insira quantos números desejar e use um array para ordená-los.
+
+import java.util.Scanner;
+
+public class OrdenacaoSimples{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Digite a quantidade de números que deseja digitar: ");
+        int qtdNum = scan.nextInt();
+        scan.nextLine();
+        int[] arrayNum = new int[qtdNum];
+
+        for(int i=0; i<qtdNum; i++){
+            System.out.print("Digite o " + (i + 1) + "º número: ");
+            arrayNum[i] = scan.nextInt();
+            scan.nextLine();
+        }
+        System.out.print("Array Digitado: ");
+        for(int num : arrayNum){
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        for(int i=0; i<qtdNum - 1; i++){
+            for(int j=0; j<qtdNum - 1; j++){
+                if(arrayNum[j] > arrayNum[j+1]){
+                    int temporaria = arrayNum[j];
+                    arrayNum[j] = arrayNum[j+1];
+                    arrayNum[j+1] = temporaria;
+                }
+            }
+        }
+
+        System.out.print("Array Ordenado de forma crescente: ");
+        for(int num : arrayNum){
+            System.out.print(num + " ");
+        }
+
+        scan.close();
+    }
+}
