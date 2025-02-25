39465433feb1a0e221a0c6d6204c49aa97f388b4
Francisco Marques <francisco.marques@pearson.com>
2025-02-25 16:05:44 -0300

    Atualização exercicio Banco

--- udemyJava/banco/src/application/Banco.java
+++ udemyJava/banco/src/application/Banco.java
@@ -1,4 +1,5 @@
 package application;
+
 import entities.Cliente;
 import java.util.Scanner;
 import java.util.Random;
@@ -9,7 +10,7 @@ public class Banco {
 		Scanner scan = new Scanner(System.in);
 		char opcaoDeposito;
 		Cliente user;
-		
+
 		System.out.print("Enter Account Number: ");
 		int conta = scan.nextInt();
 		scan.nextLine();
@@ -17,13 +18,13 @@ public class Banco {
 		String nome = scan.nextLine();
 		System.out.print("Is There an Initial Deposit (y/n)? ");
 		opcaoDeposito = scan.nextLine().charAt(0);
-		
-		while(Character.toLowerCase(opcaoDeposito) != 'y' && Character.toLowerCase(opcaoDeposito) != 'n') {
+
+		while (Character.toLowerCase(opcaoDeposito) != 'y' && Character.toLowerCase(opcaoDeposito) != 'n') {
 			System.out.print("Digite uma opção válida: ");
 			opcaoDeposito = scan.nextLine().charAt(0);
 		}
-		
-		if(Character.toLowerCase(opcaoDeposito) == 'y') {
+
+		if (Character.toLowerCase(opcaoDeposito) == 'y') {
 			System.out.print("Enter initial deposit value: ");
 			double deposito = scan.nextDouble();
 			user = new Cliente(conta, nome, deposito);
@@ -31,23 +32,23 @@ public class Banco {
 		} else {
 			user = new Cliente(conta, nome);
 		}
-		
+
 		System.out.println("Account data:");
-		//user.toPrint();
+		// user.toPrint();
 		System.out.println(user);
-		
+
 		System.out.print("\nEnter a deposit value: ");
 		user.deposito(scan.nextDouble());
 		System.out.println("Update account data:");
-		//user.toPrint();
+		// user.toPrint();
 		System.out.println(user);
 		System.out.println();
-		
+
 		System.out.print("Enter a withdrow value: ");
 		user.saque(scan.nextDouble());
-		
+
 		System.out.println("Update account data:");
-		//user.toPrint();
+		// user.toPrint();
 		System.out.println(user);
 
 		scan.close();
